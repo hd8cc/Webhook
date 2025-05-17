bbd9af4dbe01609e2c7b874773ad15730fcf68b5
hd8 <seliwarev1@gmail.com>
2025-05-17 16:06:25 +0200

    Update WebHookSender.py

--- main/WebHookSender.py
+++ main/WebHookSender.py
@@ -11,11 +11,6 @@ import socket
 index = []
 module = ["webhook.py", "user.py", "install.bat"]
 local_ip = socket.gethostbyname(socket.gethostname())
-
-
-    
-
-client = http.client.HTTPSConnection(url)
 name = Path('WebHookName').read_text()
 text = Path('Content').read_text()
 url = "discord.com"
@@ -30,7 +25,7 @@ data = {
 }
 
 
-
+client = http.client.HTTPSConnection(url)
 json_data = json.dumps(data)
 client.request("POST", id, body=json_data, headers=headers)
 response = client.getresponse()
